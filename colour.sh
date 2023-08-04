79309c826e0e792182d00a3ce103635a9efebf98
premsagarm <premsagarm9999@gmail.com>
2023-08-04 11:39:02 +0530

    done

--- colour.sh
+++ colour.sh
@@ -6,4 +6,4 @@ echo -e "\e[33m  hello this is red \e[0m"
 echo -e "\e[34m hello \e[0m"
 echo -e "\e[35m xyazzzzzzzz \e[0m"
 
-echo -e "\e[44:33m "hello world" \e[0m"
+echo -e "\e[44:33m hello world \e[0m"
