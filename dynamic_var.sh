26851b7a41a2781d6ada85927701763c418018ba
premsagarm <premsagarm9999@gmail.com>
2023-08-05 10:27:38 +0530

    done

--- dynamic_var.sh
+++ dynamic_var.sh
@@ -1,4 +1,4 @@
 date="$(date +%D)"
 session_count=$(who | wc -l)
-echo -e"\e[32mtoday date is $date\e[0m]"
+echo -e "\e[32m today date is $date \e[0m]"
 echo "Number of users joined seesion are $session_count"
