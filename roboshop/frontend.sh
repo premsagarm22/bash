ff7687290f7976399629e17b8a4a765bbd6cef6a
premsagarm <premsagarm9999@gmail.com>
2023-08-10 13:28:14 +0530

    done

--- roboshop/frontend.sh
+++ roboshop/frontend.sh
@@ -4,8 +4,10 @@ echo "i am frontend"
 
 yum install nginx -y
 
-user_id =$(id -u)
-if [[ $user_id -eq 0 ]]; then
+user_id=$(id -u)
+
+if [[ $user_id -eq 0 ]]
+then
   echo "you have signed ad root user"
 else
   echo "kindly switch into root user to make changes"
