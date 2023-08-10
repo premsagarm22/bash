5e80dbe29360f1a3f1d27a91e643b8e3bb1eabdb
premsagarm <premsagarm9999@gmail.com>
2023-08-10 11:42:11 +0530

    done

---
+++ roboshop/frontend.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "i am frontend"
+
+ yum install nginx -y
+# systemctl enable nginx 
+# systemctl start nginx 
+
+
+# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+
+# cd /usr/share/nginx/html
+# rm -rf * 
+# unzip /tmp/frontend.zip
+# mv frontend-main/* .
+# mv static/* .
+# rm -rf frontend-master README.md
+# mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
+
+# systemctl restart nginx 
