ef98f142e98e5bce06c047c7a9f17624a8f4c945
premsarja <premsagar@prodot.in>
2023-08-12 13:14:05 +0530

    done

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,22 +1,9 @@
 #!/bin/bash
 
 echo "i am frontend"
-
+echo " looking for frontend"
 user_id=$(id -u)
 
 if [ $user_id -ne 0 ]; then
-  echo -e "\e[32m you needs to be sudo user \t Example: sudo <filename>\e[0m]"
-# yum install nginx -y
-# systemctl enable nginx 
-# systemctl start nginx 
-
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-# cd /usr/share/nginx/html
-# rm -rf * 
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-master README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-# systemctl restart nginx
+  echo -e "\e[32m you needs to be sudo user \t Example: sudo <filename> \e[0m"
+  echo "hi"
--- roboshop/components/mongodb.sh
+++ roboshop/components/mongodb.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 echo "i am mongodb"
+sdss
