6df62e06c6540842cadd7cc4d646beb069f623fd
premsarja <premsagar@prodot.in>
2023-08-12 13:32:02 +0530

    done

--- roboshop/components/frontend.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "i am frontend"
-echo "looking for frontend"
-user_id=$(id -u)
-
-if [ $user_id -ne 0 ]; t
-hen
-echo -e "\e[32m you needs to be sudo user Example: sudo <filename> \e[0m"
