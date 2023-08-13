d849464d156e48350bdd67424aa413dd7c64c643
premsarja <premsagar@prodot.in>
2023-08-13 14:31:16 +0530

    f

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -3,6 +3,7 @@
 echo "starting frontend"
 
 user_id=$(id -u)
+
 if [ $user_id -ne 0 ]; then
   echo -e "\e[33m you needs to be root user to perform this\e[0m] \n\t\t Example: sudo <filename>"
   exit 1
@@ -10,10 +11,27 @@ fi
 
 echo "installing nginx"
 
-# #yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
-# exit 2
+yum install nginx -y
+systemctl enable nginx
+systemctl start nginx
+exit 2
+
+if [ $user_id -ne 0 ]; then
+  echo -e "\e[33m you needs to be root user to perform this\e[0m] \n\t\t Example: sudo <filename>"
+  exit 3
+fi  
+
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+
+cd /usr/share/nginx/html
+rm -rf *
+unzip /tmp/frontend.zip
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+exit 4
+
 
 <<comm
 # yum install nginx -y
