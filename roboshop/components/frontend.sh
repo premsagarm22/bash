19282447cf7bfea8eb9f38c73b82fd0d97469d29
premsarja <premsagar@prodot.in>
2023-08-16 19:38:59 +0530

    f

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,26 +1,10 @@
-#!/bin/bash
 
-echo "starting frontend"
-
-user_id=$(id -u)
-
-if [ $user_id -ne 0 ]; then
-  echo -e "\e[33m you needs to be root user to perform this\e[0m] \n\t\t Example: sudo <filename>"
-  exit 1
-fi  
-
-echo "installing nginx"
-
-yum install nginx -y
+sudo yum install nginx -y
 systemctl enable nginx
 systemctl start nginx
-exit 2
 
-echo "downloading code"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-echo "adding content to web"
 
 cd /usr/share/nginx/html
 rm -rf *
@@ -29,22 +13,3 @@ mv frontend-main/* .
 mv static/* .
 rm -rf frontend-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-echo "your frontend installed sucessfully"
-
-<<comm
-# yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
-
-
-# curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
-
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/frontend.zip
-# mv frontend-main/* .
-# mv static/* .
-# rm -rf frontend-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-comm
