37306f4e0748049f9930e5a135b5d7f32f75f78a
premsarja <premsagar@prodot.in>
2023-08-16 19:39:53 +0530

    f

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -4,7 +4,7 @@ systemctl enable nginx
 systemctl start nginx
 
 
-curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
+sudo curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 cd /usr/share/nginx/html
 rm -rf *
