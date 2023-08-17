51f280e1a9fbef0d58f285e9b24dc65d2af49d7e
premsarja <premsagar@prodot.in>
2023-08-17 15:44:01 +0530

    done

---
+++ roboshop/component/frontend.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+user_id=$(id -u)
+
+if [ $user_id -ne 0 ]; then
+  echo -e "/e[32m script is executed by the root user or with sudo privilege \e[0m"
+
+fi
+echo "configuring frontend"
+
