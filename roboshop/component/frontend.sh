d6893b64230d2048eba30402c3d74e2443e5fd03
premsarja <premsagar@prodot.in>
2023-08-18 15:47:08 +0530

    done

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -10,30 +10,28 @@ fi
 echo -e "\e[35m configuring frontend \e[0m \n"
 echo -n "installing frontend :"
 yum install nginx -y >> /tmp/frontend.log
-if [ $? -eq 0 ]; then
-  echo -e "\e[33m sucessfully installed \e[0m"
-else
-  echo -e "\e[31m failed \e[0m"  
-fi
-
-echo -n "starting nginx"
-systemctl enable nginx
-systemctl start nginx
 
 statusfunction(){
 
-    if [ $? -eq 0 ]; then
+    if [ $1 -eq 0 ]; then
        echo -e "\e[33m sucessfully installed \e[0m"
     else
        echo -e "\e[31m failed \e[0m"  
     fi
 }
 
+
+echo -n "starting nginx"
+systemctl enable nginx
+systemctl start nginx
+
+statusfunction $?
+
 echo -n "downloading the frontend component"
 
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
-statusfunction()
+statusfunction $?
 
 # if [ $? -eq 0 ]; then
 #   echo -e "\e[33m sucessfully installed \e[0m"
