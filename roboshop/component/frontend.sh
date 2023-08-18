d53c67ddb9ec70970232871cc70d7b9d90f16631
premsarja <premsagar@prodot.in>
2023-08-18 15:00:57 +0530

    done

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -8,12 +8,11 @@ if [ $user_id -ne 0 ]; then
 fi
 
 echo -e "\e[35m configuring frontend \e[0m \n"
-echo -n "installing frontend "
+echo -n "installing frontend :"
 yum install nginx -y
 if [ $? -eq 0 ]; then
   echo -e "\e[33m sucessfully installed \e[0m"
 else
   echo -e "\e[38m failed \e[0m"  
 fi
-echo $?
 
