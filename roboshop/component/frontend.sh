6bb05443e7b4b1f8896f2e594510a26072d5d769
premsarja <premsagar@prodot.in>
2023-08-18 16:08:56 +0530

    done

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -1,6 +1,8 @@
 #!/bin/bash 
 
 user_id=$(id -u)
+user_id=$(id -u)
+
 
 if [ $user_id -ne 0 ]; then
   echo -e "\e[32m script is executed by the root user or with sudo privilege \e[0m"
@@ -14,7 +16,7 @@ yum install nginx -y >> /tmp/frontend.log
 
 statusfunction(){
 
-    if [ $1 -eq 0 ]; then
+    if [ $2 -eq 0 ]; then
        echo -e "\e[33m sucessfully installed \e[0m"
     else
        echo -e "\e[31m failed \e[0m"  
