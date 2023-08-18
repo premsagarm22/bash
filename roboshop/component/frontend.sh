956bfd67f97fd052a4f4cf8cf7c0b01998130191
premsarja <premsagar@prodot.in>
2023-08-18 16:24:00 +0530

    done

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -14,7 +14,7 @@ yum install nginx -y >> /tmp/frontend.log
 
 statusfunction(){
 
-    if [ $1 -ne 1000 ]; then
+    if [ $1 -eq 0 ]; then
        echo -e "\e[33m sucessfully installed \e[0m"
     else
        echo -e "\e[31m failed \e[0m"  
