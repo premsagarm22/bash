f863d917da49154e81ba8cba47fe0558138943a3
premsarja <premsagar@prodot.in>
2023-08-18 16:22:56 +0530

    done

--- roboshop/component/frontend.sh
+++ roboshop/component/frontend.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
 
-# user_id=$(id -u)
-
-usr_id=$(id -u)
-
+user_id=$(id -u)
 
 if [ $user_id -ne 0 ]; then
   echo -e "\e[32m script is executed by the root user or with sudo privilege \e[0m"
@@ -17,7 +14,7 @@ yum install nginx -y >> /tmp/frontend.log
 
 statusfunction(){
 
-    if [ $2 -eq 0 ]; then
+    if [ $1 -eq 1000 ]; then
        echo -e "\e[33m sucessfully installed \e[0m"
     else
        echo -e "\e[31m failed \e[0m"  
@@ -38,6 +35,23 @@ echo -n "downloading the frontend component"
 curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/frontend/archive/main.zip"
 
 statusfunction $?
+
+echo -n "cleanining frontend "
+
+cd /usr/share/nginx/html
+rm -rf *
+
+statusfunction $?
+
+echo -n "unziping frontend"
+unzip /tmp/frontend.zip
+
+statusfunction $?
+mv frontend-main/* .
+mv static/* .
+rm -rf frontend-main README.md
+mv localhost.conf /etc/nginx/default.d/roboshop.conf
+
 
 # if [ $? -eq 0 ]; then
 #   echo -e "\e[33m sucessfully installed \e[0m"
