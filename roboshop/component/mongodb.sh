abb9d5ba6082bb053cf3f82e249164fcef847545
premsarja <premsagar@prodot.in>
2023-08-21 13:47:16 +0530

    done

--- roboshop/component/mongodb.sh
+++ roboshop/component/mongodb.sh
@@ -34,7 +34,7 @@ echo -n "installing ${component} :  "
 yum install -y mongodb-org 
 statusfunction $?
 
-echo -n "Enabling the ${COMPONENT} visibility :"
+echo -n "Enabling the ${component} visibility :"
 sed  -ie 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 statusfunction $?
 
@@ -44,21 +44,21 @@ systemctl start mongod
 statusfunction $?
 
 
-echo -n "Downloading the ${COMPONENT} schema: "
-curl -s -L -o /tmp/${COMPONENT}.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+echo -n "Downloading the ${component} schema: "
+curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/${component}/archive/main.zip" 
 statusfunction $?
 
-echo -n "Extracing the ${COMPONENT} Schema:"
+echo -n "Extracing the ${component} Schema:"
 cd /tmp 
-unzip -o ${COMPONENT}.zip &>> ${LOGFILE} 
+unzip -o ${component}.zip &>> ${LOGFILE} 
 statusfunction $?
 
 
-echo -n "Injecting ${COMPONENT} Schema:"
-cd ${COMPONENT}-main
+echo -n "Injecting ${component} Schema:"
+cd ${component}-main
 mongo < catalogue.js    &>>  ${LOGFILE}
 mongo < users.js        &>>  ${LOGFILE}
 statusfunction $?
 
-echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
+echo -e "\e[35m ${component} Installation Is Completed \e[0m \n"
 set -x
