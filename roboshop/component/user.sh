4d181e483646da612ec9fb4816a407393700acc1
premsarja <premsagar@prodot.in>
2023-08-22 12:34:53 +0530

    done

--- roboshop/component/user.sh
+++ roboshop/component/user.sh
@@ -65,7 +65,7 @@ statusfunction $?
 
 
 echo -n "starting the catalogue service: "
-mv /home/roboshop/user/systemd.service /etc/systemd/system/user.service
+mv /home/roboshop/user/user/systemd.service /etc/systemd/system/user.service
 systemctl daemon-reload
 systemctl enable user
 systemctl restart user
