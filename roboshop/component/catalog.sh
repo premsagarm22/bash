67e16b9c82038fd969192653e145f56cd54e4f81
premsarja <premsagar@prodot.in>
2023-08-23 17:54:02 +0530

    done

--- roboshop/component/catalog.sh
+++ roboshop/component/catalog.sh
@@ -61,7 +61,7 @@ npm install
 statusfunction $?
 
 echo -n "updating the ${component} systemfile "
-sed -i "s/MONGO_DNSNAME/mongodb.roboshop-internal/g" /home/${appuser}/${component}/systemd.service
+sed -i "s/MONGO_DNSNAME/mongodb.roboshop-internal/g" /home/roboshop/catalogue/catalogue-main
 mv /home/roboshop/catalogue/systemd.service /etc/systemd/system/catalogue.service
 statusfunction $?
 
