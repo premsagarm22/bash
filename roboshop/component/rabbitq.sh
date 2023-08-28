f5b5631e180ad8402d8cbb690a1a6d3372cd2491
premsarja <premsagar@prodot.in>
2023-08-28 14:20:05 +0530

    done

--- roboshop/component/rabbitq.sh
+++ roboshop/component/rabbitq.sh
@@ -2,36 +2,36 @@
 
 set -e
 # Validate the user who is running the script is a root user or not.
-COMPONENT=rabbitmq
+component=rabbitmq
 
 source components/common.sh
 
-echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
+echo -e "\e[35m Configuring ${component} ......! \e[0m \n"
 
-echo -n "Configuring ${COMPONENT} repositories:"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> ${LOGFILE}
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> ${LOGFILE}
+echo -n "Configuring ${component} repositories:"
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> ${log}
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash  &>> ${log}
 stat $? 
 
-echo -n "Installing ${COMPONENT} :"
-yum install rabbitmq-server -y  &>> ${LOGFILE}
+echo -n "Installing ${component} :"
+yum install rabbitmq-server -y  &>> ${log}
 stat $? 
 
-echo -n "Starting ${COMPONENT}:"
-systemctl enable rabbitmq-server   &>> ${LOGFILE}
-systemctl start rabbitmq-server    &>> ${LOGFILE}
+echo -n "Starting ${component}:"
+systemctl enable rabbitmq-server   &>> ${log}
+systemctl start rabbitmq-server    &>> ${log}
 stat $? 
 
-sudo rabbitmqctl list_users | grep roboshop &>> ${LOGFILE}
+sudo rabbitmqctl list_users | grep roboshop &>> ${log}
 if [ $? -ne 0 ] ; then 
-    echo -n "Creating ${COMPOMENT} user account :"
-    rabbitmqctl add_user roboshop roboshop123 &>> ${LOGFILE}
+    echo -n "Creating ${component} user account :"
+    rabbitmqctl add_user roboshop roboshop123 &>> ${log}
     stat $? 
 fi 
 
 echo -n "Configuring the permissions :"
-rabbitmqctl set_user_tags roboshop administrator     &>> ${LOGFILE}
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"     &>> ${LOGFILE}
+rabbitmqctl set_user_tags roboshop administrator     &>> ${log}
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"     &>> ${log}
 stat $?
 
-echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
+echo -e "\e[35m ${component} Installation Is Completed \e[0m \n"
