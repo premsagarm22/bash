fcd8e7abdb244fddd7684af6700f1c419c019cc6
premsarja <premsagar@prodot.in>
2023-08-31 19:07:04 +0530

    done

--- roboshop/component/common.sh
+++ roboshop/component/common.sh
@@ -12,6 +12,15 @@ if [ $user_id -ne 0 ]; then
   exit 1
 fi
 
+statusfunction() {
+    if [ $? -eq 0 ]; then 
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi
+}
+
 
 statusfunction() {
 
