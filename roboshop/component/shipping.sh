3e6b9ada6e14ec33a0758d34eb4097626ef957f5
premsarja <premsagar@prodot.in>
2023-09-01 23:51:17 +0530

    done

--- roboshop/component/shipping.sh
+++ roboshop/component/shipping.sh
@@ -78,10 +78,10 @@
 
 #!/bin/bash 
 
-COMPONENT=shipping
+component=shipping
 
 # This is how we import the functions that are declared in a different file using source 
 source component/common.sh
 JAVA                       # calling nodejs function.
 
-echo -e "\n \e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
+echo -e "\n \e[35m ${component} Installation Is Completed \e[0m \n"
