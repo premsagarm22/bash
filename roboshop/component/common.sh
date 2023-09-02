86ccf5a945d633c99fdf37185b0a38f8cbf541e2
premsarja <premsagar@prodot.in>
2023-09-02 13:59:14 +0530

    done

--- roboshop/component/common.sh
+++ roboshop/component/common.sh
@@ -6,9 +6,9 @@ set -e
 APPUSER=roboshop
 LOGFILE="/tmp/${COMPONENT}.log"
 
-user_id=$(id -u)
+USER_ID=$(id -u)
 
-if [ $user_id -ne 0 ]; then
+if [ $USER_ID -ne 0 ]; then
   echo -e "\e[32m script is executed by the root user or with sudo privilege \e[0m"
   exit 1
 fi
