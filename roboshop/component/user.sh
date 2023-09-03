01768219755f538efc691936a4af158b681fefad
premsarja <premsagar@prodot.in>
2023-09-03 21:55:38 +0530

    done

--- roboshop/component/user.sh
+++ roboshop/component/user.sh
@@ -25,7 +25,7 @@ statusfunction(){
 echo -e "\e[35m configuring t}${COMPONENT} \e[0m"
 
 echo -n "installing ${COMPONENT} :"
-curl --silent --location yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y | sudo bash -
+curl --silent --location -o yum install https://rpm.nodesource.com/pub_16.x/nodistro/repo/nodesource-release-nodistro-1.noarch.rpm -y | sudo bash -
 
 echo -n "installing Nodejs :"
 yum install nodejs -y
