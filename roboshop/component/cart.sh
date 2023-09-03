9a9733b96f33071486d10abc9a39dd3c582e10c4
premsarja <premsagar@prodot.in>
2023-09-03 21:13:38 +0530

    done

--- roboshop/component/cart.sh
+++ roboshop/component/cart.sh
@@ -61,7 +61,7 @@
 COMPONENT=cart
 
 # This is how we import the functions that are declared in a different file using source 
-source components/common.sh
-NODEJS                       # calling nodejs function.
+source component/common.sh
+NODEJS # calling nodejs function.
 
 echo -e "\n \e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
