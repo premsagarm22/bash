4c88164193b442f5daf0a2d3c76106b2e6eb2f74
premsarja <premsagar@prodot.in>
2023-09-06 16:32:17 +0530

    done

--- roboshop/ec2.sh
+++ roboshop/ec2.sh
@@ -10,7 +10,7 @@ AMI_ID="ami-0c1d144c8fdd8d690"
 INSTANCE_TYPE="t3.micre"
 SECURITY_GROUP="sg-071baaff364d61305"
 COMPONENT=$1
-if [-z $1];then
+if { -z $1 ]; then
   echo -e "\e33m COMPONENT name is needed\e[0m"
   echo -e "\e[35m ex;usage $bash ec2.sh \e[0m"
   exit 1
